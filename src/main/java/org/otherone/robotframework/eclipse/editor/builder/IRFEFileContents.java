5ab41f82eed55fea5b39688d008dbcdd5fcc3dde
Jonas Berlin <xkr47@outerspace.dyndns.org>
2011-10-21 08:53:35 +0300

    Add all remaining features available from a robot file.

--- src/main/java/org/otherone/robotframework/eclipse/editor/builder/IRFEFileContents.java
+++ src/main/java/org/otherone/robotframework/eclipse/editor/builder/IRFEFileContents.java
@@ -15,10 +15,60 @@
  */
 package org.otherone.robotframework.eclipse.editor.builder;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * This interface provides bean-ish access to the parsed structure of a robot file.
+ * 
+ * @author xkr47
+ */
 public interface IRFEFileContents {
+
+  /**
+   * This is used to tag methods whose return value should always be null for resource files. I.e.
+   * the resource file is syntactically broken if the annotated method returns a non-null value.
+   */
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.METHOD)
+  public @interface NotAllowedInResourceFiles {
+    /**
+     * @return since which Robot Framework version is it not allowed? (i.e. it was allowed before
+     *         this version)
+     */
+    String value() default "";
+  }
+
+  /**
+   * This is used to tag methods whose return value should always be null for test suite
+   * initialization files, e.g. "__init__.txt".
+   */
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.METHOD)
+  public @interface NotAllowedInTestSuiteInitializationFiles {
+    /**
+     * @return since which Robot Framework version is it not allowed? (i.e. it was allowed before
+     *         this version)
+     */
+    String value() default "";
+  }
+
+  /**
+   * This is used to tag methods whose return value should always be null for robot files prior to a
+   * specific version.
+   */
+  @Retention(RetentionPolicy.RUNTIME)
+  @Target(ElementType.METHOD)
+  public @interface AvailableFrom {
+    /**
+     * @return since which Robot Framework version is it available?
+     */
+    String value();
+  }
 
   public interface IPositionedString {
 
@@ -82,7 +132,9 @@ public interface IRFEFileContents {
 
   // Setting table, suite stuff
 
-  List<IPositionedString> getResourceFiles();
+  List<IDynamicPositionedString> getResourceFiles();
+
+  Map<IDynamicPositionedString, List<IDynamicPositionedString>> getVariableFiles();
 
   /**
    * Libraries to load, mapping to their arguments. TODO a library can be loaded many times with
@@ -90,38 +142,55 @@ public interface IRFEFileContents {
    */
   Map<IDynamicPositionedString, List<IDynamicPositionedString>> getLibraryFiles();
 
-  IPositionedString getSuiteSetup();
+  @NotAllowedInResourceFiles
+  IKeywordCall getSuiteSetup();
 
-  IPositionedString getSuiteTeardown();
+  @NotAllowedInResourceFiles
+  IKeywordCall getSuiteTeardown();
 
   List<IDynamicPositionedString> getDocumentation();
 
+  @NotAllowedInResourceFiles
   Map<IPositionedString, List<IDynamicPositionedString>> getMetadata();
 
   // Setting table, test case stuff
 
+  @NotAllowedInResourceFiles
   List<IDynamicPositionedString> getForcedTestTags();
 
+  @NotAllowedInResourceFiles
+  @NotAllowedInTestSuiteInitializationFiles("2.5")
   List<IDynamicPositionedString> getDefaultTestTags();
 
+  @NotAllowedInResourceFiles
   IKeywordCall getDefaultTestSetup();
 
+  @NotAllowedInResourceFiles
   IKeywordCall getDefaultTestTeardown();
 
+  @NotAllowedInResourceFiles
+  @NotAllowedInTestSuiteInitializationFiles("2.5")
   IPositionedKeywordString getTemplate(); // not dynamic
 
-  IPositionedString getDefaultTestTimeout();
+  /**
+   * Since 2.5.6, the special keyword "NONE" can be used.
+   */
+  @NotAllowedInResourceFiles
+  @NotAllowedInTestSuiteInitializationFiles("2.5")
+  IDynamicPositionedString getDefaultTestTimeout();
 
+  @NotAllowedInResourceFiles
+  @NotAllowedInTestSuiteInitializationFiles("2.5")
   IPositionedString getDefaultTestTimeoutMessage();
 
   // Other tables
 
-  public interface IKeywordSequence {
+  public interface ITestCaseDefinition extends IKeywordSequence {
 
-    IDynamicPositionedString getSequenceName(); // TODO just IPositionedString for test cases?
-
-    List<IDynamicPositionedString> getDocumentation();
-
+    /**
+     * @return may return the special keyword "NONE" to override suite-default tags declared with
+     *         {@link IRFEFileContents#getDefaultTestTags()}.
+     */
     List<IDynamicPositionedString> getDeclaredStaticTags();
 
     /**
@@ -137,9 +206,9 @@ public interface IRFEFileContents {
     List<IDynamicPositionedString> getDeclaredAndInheritedStaticTags();
 
     /**
-     * The {@link IKeywordCall#getKeyword()} may exceptionally return one of the special values ""
-     * or "NONE" when the user wants to override the file-default test setup with a no-op. (It's
-     * unknown why the "No Operation" keyword wasn't reused).
+     * Since Robot Framework 2.5.6, the {@link IKeywordCall#getKeyword()} may exceptionally return
+     * one of the special values "" or "NONE" when the user wants to override the file-default test
+     * setup with a no-op. (It's unknown why the "No Operation" keyword wasn't reused).
      */
     IKeywordCall getTestSetup();
 
@@ -148,20 +217,48 @@ public interface IRFEFileContents {
      */
     IKeywordCall getTestTeardown();
 
+    /**
+     * Since Robot Framework 2.5.6, the special template "NONE" indicates no template should be
+     * used.
+     */
     IPositionedKeywordString getTemplate(); // not dynamic
 
-    IPositionedString getTestTimeout();
+  }
 
-    IPositionedString getTestTimeoutMessage();
+  public interface IUserKeywordDefinition extends IKeywordSequence {
+
+    /**
+     * When arguments have default values, {@link IDynamicPositionedString#getParts()} returns two
+     * parts, "${argument}" and "=value".
+     */
+    List<IDynamicPositionedString> getArguments();
+
+    List<IDynamicPositionedString> getReturnValues();
+
+    @AvailableFrom("2.6")
+    IKeywordCall getKeywordTeardown();
+
+  }
+
+  public interface IKeywordSequence {
+
+    IDynamicPositionedString getSequenceName(); // TODO just IPositionedString for test cases?
+
+    List<IDynamicPositionedString> getDocumentation();
+
+    /**
+     * Since 2.5.6, the special keyword "NONE" can be used.
+     */
+    IDynamicPositionedString getTimeout();
+
+    IPositionedString getTimeoutMessage();
 
     List<IKeywordCall> getKeywordCalls();
   }
 
   public interface IKeywordCall {
     /**
-     * Returns null if a template keyword is active
-     * 
-     * @return
+     * Returns null if a template keyword is active, except if the keyword is ":FOR".
      */
     IDynamicPositionedKeywordString getKeyword();
 
@@ -170,8 +267,8 @@ public interface IRFEFileContents {
 
   Map<IPositionedString, IDynamicPositionedString> getVariables();
 
-  Map<IDynamicPositionedString, IKeywordSequence> getTestCases();
+  Map<IPositionedString, ITestCaseDefinition> getTestCases();
 
-  Map<IDynamicPositionedString, IKeywordSequence> getKeywords();
+  Map<IDynamicPositionedString, IUserKeywordDefinition> getKeywords();
 
 }
