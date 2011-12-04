23688a3cd769ca383cba3337ff8eace3b9d5ad71
Jonas Berlin <xkr47@outerspace.dyndns.org>
2011-12-04 19:57:28 +0200

    PreParser for determining type of line

---
+++ src/main/java/org/otherone/robotframework/eclipse/editor/builder/parser/RFEPreParser.java
@@ -0,0 +1,173 @@
+/**
+ * Copyright 2011 Nitor Creations Oy
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.otherone.robotframework.eclipse.editor.builder.parser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.text.IDocument;
+import org.otherone.robotframework.eclipse.editor.structure.ParsedString;
+
+public class RFEPreParser {
+
+  private final String filename;
+  private final List<RFELine> lines;
+
+  /**
+   * For documents being edited.
+   * 
+   * @param document
+   */
+  public RFEPreParser(IDocument document, List<RFELine> lines) {
+    this.filename = "<document being edited>";
+    this.lines = lines;
+  }
+
+  public void preParse() throws CoreException {
+    try {
+      System.out.println("Preparsing " + filename);
+      for (RFELine line : lines) {
+        try {
+          parseLine(line);
+        } catch (CoreException e) {
+          throw new RuntimeException("Error when preparsing line " + line.lineNo + ": '" + line.arguments + "'", e);
+        } catch (RuntimeException e) {
+          throw new RuntimeException("Internal error when preparsing line " + line.lineNo + ": '" + line.arguments + "'", e);
+        }
+      }
+
+      // TODO store results
+    } catch (Exception e) {
+      throw new RuntimeException("Error preparsing robot file " + filename, e);
+    }
+  }
+
+  //  enum State {
+  //    IGNORE, SETTING_TABLE, VARIABLE_TABLE, TESTCASE_TABLE_INITIAL, TESTCASE_TABLE_ACTIVE, KEYWORD_TABLE_INITIAL, KEYWORD_TABLE_ACTIVE,
+  //  }
+
+  public enum Type {
+    IGNORE, SETTING_TABLE_BEGIN, SETTING_TABLE_LINE, VARIABLE_TABLE_BEGIN, VARIABLE_TABLE_LINE, TESTCASE_TABLE_BEGIN, TESTCASE_TABLE_IGNORE,
+    TESTCASE_TABLE_TESTCASE_BEGIN, TESTCASE_TABLE_TESTCASE_LINE, KEYWORD_TABLE_BEGIN, KEYWORD_TABLE_IGNORE, KEYWORD_TABLE_KEYWORD_BEGIN,
+    KEYWORD_TABLE_KEYWORD_LINE, CONTINUATION_LINE, COMMENT_LINE,
+  }
+
+  private Type prevLineType = Type.IGNORE;
+
+  private void parseLine(RFELine line) throws CoreException {
+    System.out.println(line.arguments);
+    Type tableType = tryParseTableSwitch(line);
+    if (tableType != null) {
+      line.info.put(Type.class, tableType);
+      prevLineType = tableType;
+      return;
+    }
+    if (tryParseContinuationLine(line)) {
+      line.info.put(Type.class, Type.CONTINUATION_LINE);
+      // prevLineType not updated
+      return;
+    }
+    String first = line.arguments.get(0).getValue();
+    boolean firstEmpty = first.isEmpty();
+    String second = line.arguments.size() < 2 ? "" : line.arguments.get(1).getValue();
+    if (first.startsWith("#") || firstEmpty && second.startsWith("#")) {
+      line.info.put(Type.class, Type.COMMENT_LINE);
+      // prevLineType not updated
+      return;
+    }
+    Type lineType = determineLineTypeFromPrevious(firstEmpty);
+    line.info.put(Type.class, lineType);
+    prevLineType = lineType;
+  }
+
+  private Type tryParseTableSwitch(RFELine line) throws CoreException {
+    ParsedString tableArgument = line.arguments.get(0);
+    if (!tableArgument.getValue().startsWith("*")) {
+      return null;
+    }
+    String table = tableArgument.getValue().replace("*", "");
+    Type curType = tableNameToType.get(table);
+    if (curType == null) {
+      return Type.IGNORE;
+    }
+    return curType;
+  }
+
+  private static final Map<String, Type> tableNameToType = new HashMap<String, Type>();
+
+  static {
+    tableNameToType.put("Setting", Type.SETTING_TABLE_BEGIN);
+    tableNameToType.put("Settings", Type.SETTING_TABLE_BEGIN);
+    tableNameToType.put("Metadata", Type.SETTING_TABLE_BEGIN);
+    tableNameToType.put("Variable", Type.VARIABLE_TABLE_BEGIN);
+    tableNameToType.put("Variables", Type.VARIABLE_TABLE_BEGIN);
+    tableNameToType.put("Test Case", Type.TESTCASE_TABLE_BEGIN);
+    tableNameToType.put("Test Cases", Type.TESTCASE_TABLE_BEGIN);
+    tableNameToType.put("Keyword", Type.KEYWORD_TABLE_BEGIN);
+    tableNameToType.put("Keywords", Type.KEYWORD_TABLE_BEGIN);
+    tableNameToType.put("User Keyword", Type.KEYWORD_TABLE_BEGIN);
+    tableNameToType.put("User Keywords", Type.KEYWORD_TABLE_BEGIN);
+  }
+
+  private boolean tryParseContinuationLine(RFELine line) throws CoreException {
+    ParsedString arg = line.arguments.get(0);
+    if (!arg.getValue().equals("\\")) {
+      // first column not continuation, try second-column continuation
+      if (!arg.getValue().isEmpty()) {
+        // first column must be empty for second-column continuation
+        return false;
+      }
+      if (line.arguments.size() < 2) {
+        // must have two columns 
+        return false;
+      }
+      if (!line.arguments.get(1).getValue().equals("\\")) {
+        // second column not continuation either
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private Type determineLineTypeFromPrevious(boolean firstEmpty) {
+    switch (prevLineType) {
+      case IGNORE:
+        return Type.IGNORE;
+      case SETTING_TABLE_BEGIN:
+      case SETTING_TABLE_LINE:
+        return Type.SETTING_TABLE_LINE;
+      case VARIABLE_TABLE_BEGIN:
+      case VARIABLE_TABLE_LINE:
+        return Type.VARIABLE_TABLE_LINE;
+      case TESTCASE_TABLE_BEGIN:
+      case TESTCASE_TABLE_IGNORE:
+        return !firstEmpty ? Type.TESTCASE_TABLE_TESTCASE_BEGIN : Type.TESTCASE_TABLE_IGNORE;
+      case TESTCASE_TABLE_TESTCASE_BEGIN:
+      case TESTCASE_TABLE_TESTCASE_LINE:
+        return !firstEmpty ? Type.TESTCASE_TABLE_TESTCASE_BEGIN : Type.TESTCASE_TABLE_TESTCASE_LINE;
+      case KEYWORD_TABLE_BEGIN:
+      case KEYWORD_TABLE_IGNORE:
+        return !firstEmpty ? Type.KEYWORD_TABLE_KEYWORD_BEGIN : Type.KEYWORD_TABLE_IGNORE;
+      case KEYWORD_TABLE_KEYWORD_BEGIN:
+      case KEYWORD_TABLE_KEYWORD_LINE:
+        return !firstEmpty ? Type.KEYWORD_TABLE_KEYWORD_BEGIN : Type.KEYWORD_TABLE_KEYWORD_LINE;
+    }
+    throw new RuntimeException("Unhandled previous line type " + prevLineType);
+  }
+
+}
--- src/main/java/org/otherone/robotframework/eclipse/editor/editors/RFTColoringScanner.java
+++ src/main/java/org/otherone/robotframework/eclipse/editor/editors/RFTColoringScanner.java
@@ -16,8 +16,8 @@
 package org.otherone.robotframework.eclipse.editor.editors;
 
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
@@ -25,24 +25,22 @@ import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.IToken;
 import org.eclipse.jface.text.rules.ITokenScanner;
 import org.eclipse.jface.text.rules.Token;
-import org.otherone.robotframework.eclipse.editor.builder.parser.RFELine;
 import org.otherone.robotframework.eclipse.editor.builder.parser.RFELexer;
-import org.otherone.robotframework.eclipse.editor.builder.parser.RFEParser;
-import org.otherone.robotframework.eclipse.editor.structure.api.IDynamicParsedString;
-import org.otherone.robotframework.eclipse.editor.structure.api.IParsedString;
-import org.otherone.robotframework.eclipse.editor.structure.api.IRFEFileContents;
-import org.otherone.robotframework.eclipse.editor.structure.api.IVariableDefinition;
+import org.otherone.robotframework.eclipse.editor.builder.parser.RFELine;
+import org.otherone.robotframework.eclipse.editor.builder.parser.RFEPreParser;
+import org.otherone.robotframework.eclipse.editor.structure.ParsedString;
 
 public class RFTColoringScanner implements ITokenScanner {
-
-  private IRFEFileContents fileContents;
-  private Iterator<Entry<IParsedString, IVariableDefinition>> fileContentsVariableIt;
-  private Entry<IParsedString, IVariableDefinition> entry;
-  boolean value = true;
 
   private final ColorManager manager;
   private final Token tokVARIABLE;
   private final Token tokVARIABLE_VAL;
+
+  private IDocument document;
+  private Iterator<RFELine> lineIterator;
+  private RFELine line;
+  private int argOff;
+
 
   public RFTColoringScanner(ColorManager colorManager) {
     this.manager = colorManager;
@@ -54,61 +52,166 @@ public class RFTColoringScanner implements ITokenScanner {
   @Override
   public void setRange(IDocument document, int offset, int length) {
     try {
+      this.document = document;
+      tokenQueue.reset();
       List<RFELine> lines = new RFELexer(document).lex();
-      fileContents = new RFEParser(document, lines).parse();
-      this.fileContentsVariableIt = fileContents.getVariables().entrySet().iterator();
+      new RFEPreParser(document, lines).preParse();
+      lineIterator = lines.iterator();
+      prepareNextLine();
+      // fileContents = new RFEParser(document, lines).parse();
+      // this.fileContentsVariableIt = fileContents.getVariables().entrySet().iterator();
     } catch (CoreException e) {
       throw new RuntimeException("Error parsing", e);
     }
   }
 
+  void prepareNextToken() {
+    ++argOff;
+    if (argOff >= line.arguments.size()) {
+      //prepareNextLine();
+      prepareNextLine();
+    }
+  }
+
+  void prepareNextLine() {
+    if (lineIterator.hasNext()) {
+      line = lineIterator.next();
+    } else {
+      line = null;
+    }
+    argOff = 0;
+  }
+
+  RFELine l;
+
   @Override
   public IToken nextToken() {
-    if (value) {
-      if (!fileContentsVariableIt.hasNext()) {
-        entry = null;
-        return Token.EOF;
-      }
-      entry = fileContentsVariableIt.next();
-      value = false;
-    } else {
-      value = true;
+    if (!tokenQueue.hasPending()) {
+      l = line;
+      parseMoreTokens();
     }
-    return value ? tokVARIABLE_VAL : tokVARIABLE;
+    IToken t = tokenQueue.take();
+    int tokenOff = getTokenOffset();
+    int tokenLen = getTokenLength();
+    System.out.println("TOK: " + (l != null ? "[" + l.lineNo + ":" + l.lineCharPos + "] " : "") + t + " off " + tokenOff + " end " + (tokenOff + tokenLen)
+        + " len " + tokenLen + " txt \"" + document.get().substring(tokenOff, tokenOff + tokenLen) + "\"");
+    return t;
+  }
+
+  void parseMoreTokens() {
+    if (line == null) {
+      tokenQueue.addEof();
+      return;
+    }
+    switch ((RFEPreParser.Type) line.info.get(RFEPreParser.Type.class)) {
+      case COMMENT_LINE: {
+        argOff = line.arguments.size() - 1;
+        ParsedString comment = line.arguments.get(argOff);
+        tokenQueue.add(comment.getArgCharPos(), comment.getValue().length(), tokVARIABLE);
+        prepareNextLine();
+        return;
+      }
+      default: {
+        ParsedString arg = line.arguments.get(argOff);
+        tokenQueue.add(arg.getArgCharPos(), arg.getValue().length(), tokVARIABLE_VAL);
+        prepareNextToken();
+        return;
+      }
+    }
+    //    if (value) {
+    //      if (!fileContentsVariableIt.hasNext()) {
+    //        entry = null;
+    //        return Token.EOF;
+    //      }
+    //      entry = fileContentsVariableIt.next();
+    //      value = false;
+    //    } else {
+    //      value = true;
+    //    }
+    //    return value ? tokVARIABLE_VAL : tokVARIABLE;
+  }
+
+  //  public int getTokenLength2() {
+  //    if (value) {
+  //      List<IDynamicParsedString> values = entry.getValue().getValues();
+  //      IDynamicParsedString val0 = values.get(0);
+  //      IDynamicPars7edString valn = values.get(values.size() - 1);
+  //      return valn.getArgEndCharPos() - val0.getArgCharPos();
+  //    } else {
+  //      IParsedString key = entry.getKey();
+  //      return key.getArgEndCharPos() - key.getArgCharPos();
+  //    }
+  //  }
+
+  private final TokenQueue tokenQueue = new TokenQueue();
+
+  static class TokenQueue {
+    static class PendingToken {
+      final IToken token;
+      final int len;
+
+      public PendingToken(IToken token, int len) {
+        this.token = token;
+        this.len = len;
+      }
+    }
+
+    private final List<PendingToken> pendingTokens = new LinkedList<PendingToken>();
+    private int nextTokenStart = 0;
+    private int curTokenOff, curTokenLen;
+
+    void reset() {
+      nextTokenStart = 0;
+      assert pendingTokens.isEmpty();
+      pendingTokens.clear();
+      curTokenOff = curTokenLen = 0;
+    }
+
+    IToken take() {
+      PendingToken removed = pendingTokens.remove(0);
+      curTokenOff += curTokenLen;
+      curTokenLen = removed.len;
+      return removed.token;
+    }
+
+    public void addEof() {
+      addToken(0, Token.EOF);
+    }
+
+    boolean hasPending() {
+      return !pendingTokens.isEmpty();
+    }
+
+    void add(int off, int len, IToken token) {
+      if (off > nextTokenStart) {
+        addToken(off - nextTokenStart, Token.UNDEFINED);
+      }
+      addToken(len, token);
+      nextTokenStart = off + len;
+    }
+
+    private void addToken(int len, IToken token) {
+      pendingTokens.add(new PendingToken(token, len));
+    }
+
+    public int getLastTakenTokenOffset() {
+      return curTokenOff;
+    }
+
+    public int getLastTakenTokenLength() {
+      return curTokenLen;
+    }
+
   }
 
   @Override
   public int getTokenOffset() {
-    int i = getTokenOffset2();
-    System.out.println("getTokenOffset " + i);
-    return i;
-  }
-
-  public int getTokenOffset2() {
-    if (value) {
-      return entry.getValue().getValues().get(0).getArgCharPos();
-    } else {
-      return entry.getKey().getArgCharPos();
-    }
+    return tokenQueue.getLastTakenTokenOffset();
   }
 
   @Override
   public int getTokenLength() {
-    int i = getTokenLength2();
-    System.out.println("getTokenLength " + i);
-    return i;
-  }
-
-  public int getTokenLength2() {
-    if (value) {
-      List<IDynamicParsedString> values = entry.getValue().getValues();
-      IDynamicParsedString val0 = values.get(0);
-      IDynamicParsedString valn = values.get(values.size() - 1);
-      return valn.getArgEndCharPos() - val0.getArgCharPos();
-    } else {
-      IParsedString key = entry.getKey();
-      return key.getArgEndCharPos() - key.getArgCharPos();
-    }
+    return tokenQueue.getLastTakenTokenLength();
   }
 
 }
