f5ace844b501f9b1fff7c6ac7c0c89d45f7d0527
Jonas Berlin <jonas.berlin@nitorcreations.com>
2013-09-02 22:53:00 +0300

    KeywordDefinitionAttemptVisitor: Extend tests further

--- plugin/src/test/java/com/nitorcreations/robotframework/eclipseide/internal/assistant/proposalgenerator/TestKeywordDefinitionAttemptVisitor.java
+++ plugin/src/test/java/com/nitorcreations/robotframework/eclipseide/internal/assistant/proposalgenerator/TestKeywordDefinitionAttemptVisitor.java
@@ -19,6 +19,7 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.eclipse.core.resources.IFile;
@@ -26,46 +27,63 @@ import org.eclipse.jface.text.Region;
 import org.junit.Test;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 
+import com.nitorcreations.junit.runners.NicelyParameterized;
 import com.nitorcreations.robotframework.eclipseide.internal.assistant.Content;
 import com.nitorcreations.robotframework.eclipseide.structure.ParsedString;
 import com.nitorcreations.robotframework.eclipseide.structure.ParsedString.ArgumentType;
 
 @RunWith(Enclosed.class)
 public class TestKeywordDefinitionAttemptVisitor {
+
+    @RunWith(NicelyParameterized.class)
     public static class Regular_keywords extends BaseTestAttemptVisitor {
         KeywordDefinitionAttemptVisitor visitor;
 
+        private final String table;
+        private final String callHostIdentifier;
+
+        @Parameters
+        public static List<Object[]> parameters() {
+            return Arrays.asList(new Object[][] { { "Test cases", "TEST CASE" }, { "Keywords", "KEYWORD" } });
+        }
+
+        public Regular_keywords(String table, String callHostIdentifier) {
+            this.table = table;
+            this.callHostIdentifier = callHostIdentifier;
+        }
+
         @Test
         public void should_propose_missing_keywords_in_single_file() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1\n*Keywords\nExisting keyword\n  Missing keyword 2  argument2");
+            IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1\n*Keywords\nExisting keyword\n  Missing keyword 2  argument2");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 2, proposals.size());
             assertEquals("Missing keyword 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
             assertEquals("Missing keyword 2", proposals.get(1).getMatchArgument());
-            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*Existing keyword.*"));
+            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*KEYWORD.*Existing keyword.*"));
         }
 
         @Test
         public void should_propose_keyword_at_cursor_as_missing() throws Exception {
-            Content origContents = new Content("*Test cases\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1\n*Keywords\n<arg>Existing keyword<argend>");
+            Content origContents = new Content("*" + table + "\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1\n*Keywords\n<arg>Existing keyword<argend>");
             IFile origFile = addFile("orig.txt", origContents.c());
             visitor = new KeywordDefinitionAttemptVisitor(origFile, origContents.ps("arg-argend", 0, ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 2, proposals.size());
             assertEquals("Existing keyword", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
             assertEquals("Missing keyword 1", proposals.get(1).getMatchArgument());
-            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_not_propose_locally_missing_keywords_actually_found_in_included_file() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Settings\nResource  other.txt\n*Test cases\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
+            IFile origFile = addFile("orig.txt", "*Settings\nResource  other.txt\n*" + table + "\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
             IFile otherFile = addFile("other.txt", "*Keywords\nExisting keyword\n");
             when(resourceManager.getRelativeFile(origFile, "other.txt")).thenReturn(otherFile);
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
@@ -73,24 +91,24 @@ public class TestKeywordDefinitionAttemptVisitor {
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 1, proposals.size());
             assertEquals("Missing keyword 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_not_propose_locally_missing_keywords_actually_found_in_included_library() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Settings\nLibrary  MyLibrary\n*Test cases\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
+            IFile origFile = addFile("orig.txt", "*Settings\nLibrary  MyLibrary\n*" + table + "\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
             addLibraryIndex("MyLibrary", "Existing keyword\n");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 1, proposals.size());
             assertEquals("Missing keyword 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_not_propose_keywords_missing_from_included_file() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Settings\nResource  other.txt\n*Test cases\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
+            IFile origFile = addFile("orig.txt", "*Settings\nResource  other.txt\n*" + table + "\nTestCase\n  Existing keyword\n  Missing keyword 1  argument1");
             IFile otherFile = addFile("other.txt", "*Keywords\nExisting keyword\n  Missing keyword 2  argument2\n");
             when(resourceManager.getRelativeFile(origFile, "other.txt")).thenReturn(otherFile);
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
@@ -98,41 +116,64 @@ public class TestKeywordDefinitionAttemptVisitor {
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 1, proposals.size());
             assertEquals("Missing keyword 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_not_propose_builtin_keyword() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  " + BUILTIN_KEYWORD + "\n  Missing keyword 1  argument1");
+            IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  " + BUILTIN_KEYWORD + "\n  Missing keyword 1  argument1");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 1, proposals.size());
             assertEquals("Missing keyword 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_filter_missing_keywords_by_attempted_prefix() throws Exception {
-            Content origContents = new Content("*Test cases\nTestCase\n  Existing keyword\n  Keyword missing 1  argument1\n  Keyword missing 2\n  Keyword existing\n  Non-existent but mismatching prefix\n*Keywords\nKeyword existing\n<arg>Keyword <cursor>missing 1<argend>");
+            Content origContents = new Content("*" + table + "\nTestCase\n  Existing keyword\n  Keyword missing 1  argument1\n  Keyword missing 2\n  Keyword existing\n  Non-existent but mismatching prefix\n*Keywords\nKeyword existing\n<arg>Keyword <cursor>missing 1<argend>");
             IFile origFile = addFile("orig.txt", origContents.c());
             visitor = new KeywordDefinitionAttemptVisitor(origFile, origContents.ps("arg-argend", 0, ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt(origContents.s("arg-cursor").toLowerCase(), new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 2, proposals.size());
             assertEquals("Keyword missing 1", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
             assertEquals("Keyword missing 2", proposals.get(1).getMatchArgument());
-            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(1).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
+        }
+
+        @Test
+        public void should_not_propose_keyword_definition_for_premature_keyword_calls() throws Exception {
+            IFile origFile = addFile("orig.txt", "*" + table + "\n  Premature keyword");
+            visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
+            RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
+            List<RobotCompletionProposal> proposals = proposalSet.getProposals();
+            assertEquals(proposals.toString(), 0, proposals.size());
         }
     }
 
+    @RunWith(NicelyParameterized.class)
     public static class Wildcard_keywords extends BaseTestAttemptVisitor {
         KeywordDefinitionAttemptVisitor visitor;
 
+        private final String table;
+        private final String callHostIdentifier;
+
+        @Parameters
+        public static List<Object[]> parameters() {
+            return Arrays.asList(new Object[][] { { "Test cases", "TEST CASE" }, { "Keywords", "KEYWORD" } });
+        }
+
+        public Wildcard_keywords(String table, String callHostIdentifier) {
+            this.table = table;
+            this.callHostIdentifier = callHostIdentifier;
+        }
+
         @Test
         public void should_not_propose_regular_call_matched_by_wildcard_keyword_definition() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  Say hello\n*Keywords\nSay ${something}");
+            IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  Say hello\n*Keywords\nSay ${something}");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
@@ -141,18 +182,18 @@ public class TestKeywordDefinitionAttemptVisitor {
 
         @Test
         public void should_not_propose_regular_calls_matched_by_wildcard_keyword_definitions_mixed_with_regular() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  Say hello\n  Missing keyword\n  Existing keyword\n*Keywords\nSay ${something}\n${action} database\nExisting keyword\n");
+            IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  Say hello\n  Missing keyword\n  Existing keyword\n*Keywords\nSay ${something}\n${action} database\nExisting keyword\n");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
             assertEquals(proposals.toString(), 1, proposals.size());
             assertEquals("Missing keyword", proposals.get(0).getMatchArgument());
-            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*TestCase.*"));
+            assertTrue(proposals.get(0).getAdditionalProposalInfo().matches(".*Called from.*" + callHostIdentifier + ".*TestCase.*"));
         }
 
         @Test
         public void should_not_propose_wildcard_call_exactly_matched_by_wildcard_keyword_definition() throws Exception {
-            IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  Log in as ${user} with password ${password}\n*Keywords\nLog in as ${u} with password ${p}");
+            IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  Log in as ${user} with password ${password}\n*Keywords\nLog in as ${u} with password ${p}");
             visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("", 0).setType(ArgumentType.NEW_KEYWORD));
             RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
             List<RobotCompletionProposal> proposals = proposalSet.getProposals();
@@ -162,7 +203,7 @@ public class TestKeywordDefinitionAttemptVisitor {
         // TODO
         // @Test
         // public void should_gently_propose_wildcard_calls_matched_by_regular_keyword_definitions() throws Exception {
-        // IFile origFile = addFile("orig.txt", "*Test cases\nTestCase\n  Say ${key}\n*Keywords\nSay hello");
+        // IFile origFile = addFile("orig.txt", "*" + table + "\nTestCase\n  Say ${key}\n*Keywords\nSay hello");
         // visitor = new KeywordDefinitionAttemptVisitor(origFile, new ParsedString("",
         // 0).setType(ArgumentType.NEW_KEYWORD));
         // RobotCompletionProposalSet proposalSet = visitor.visitAttempt("", new Region(0, 0));
